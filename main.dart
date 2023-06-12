04bb65d2c0aae7663464d6ae414075c2b91244c1
abdulrazaq-01 <103704049+abdulrazaq-01@users.noreply.github.com>
2023-06-12 16:34:11 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,15 @@
+
+import 'dart:io';
+import 'dart:math';
+
+import 'Human.dart';
+
+void main(List<String> args) {
+  Human Ahmed = Human(weight: 77,haircolor: 'black',height: 1.75,numberOfArms:1);
+  Ahmed.numberOfArms=0;
+  print(Ahmed.numberOfArms);
+  print(Ahmed.haircolor);
+
+  
+
+}
